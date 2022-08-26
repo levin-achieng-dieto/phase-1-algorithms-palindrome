58eda9f92ade83dac18271b95f57b0356502d2d2
levin-achieng-dieto <levin.dieto@student.moringaschool.com>
2022-08-26 23:50:03 +0300

    assignment

--- index.js
+++ index.js
@@ -1,14 +1,31 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  word = word.replace(/\W/g, '')
+  word = word.toLowerCase();
+  return word === word.slpit('').reverse().join('');
 }
+console.log(isPalindrome(word));
 
 /* 
   Add your pseudocode here
+  Start
+input;
+if input.split('')===input.split('').reverse().join;
+return true;
+else return false
+end
 */
+
+
 
 /*
   Add written explanation of your solution here
+  the user key in Data, the program therefore 
+splits data into characters. the splited data is 
+compared with the reverse of itselt, if both the indexes of the string matches,
+the return value is true, else the return value is false
 */
+
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
